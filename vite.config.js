b850e1081394ecddb60ce35e9be266112127601d
MontoyaCl3 <cmontoya2739@gmail.com>
2025-04-24 10:08:44 -0500

    Prueba de GithubPages

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "https://montoyacl3.github.io/E-Commerce/"
+  base:"https://montoyacl3.github.io/E-Commerce/"
 })
 
